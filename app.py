3a67511e8754de5e883218743737d197016480d5
Leandro Gonçalves <leandroaugusto@ioasys.com.br>
2023-08-08 22:28:24 -0300

    testing streaminlit

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
+# app.py
+import streamlit as st
 
+def main():
+    st.title('Meu Primeiro Aplicativo Streamlit')
+    st.subheader('Este é um projeto de TCC para análise de sentimentos em postagens de redes sociais.')
+
+    if st.button('Clique aqui'):
+        st.write('Você clicou no botão!')
+
+if __name__ == "__main__":
+    main()
