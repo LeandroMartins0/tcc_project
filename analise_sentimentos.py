b94938533d094f7a9fd502ed73c967bd45dd93db
Leandro Gonçalves <leandroaugusto@ioasys.com.br>
2023-08-21 20:40:46 -0300

    1

--- analise_sentimentos.py
+++ analise_sentimentos.py
@@ -47,6 +47,29 @@ def show(df):
     # Gráficos (Aqui você pode adicionar gráficos para visualizar distribuição, tendências, etc.)
     st.header("Detalhes dos Sentimentos")
     
+    # Gráficos de barra mostrando a distribuição dos sentimentos
+    st.subheader("Distribuição dos Sentimentos")
+    sentiments = ['positivo', 'neutro', 'negativo']
+    sentiment_counts = [len(df[df['sentimento'] == sentiment]) for sentiment in sentiments]
+    
+    fig, ax = plt.subplots()
+    ax.bar(sentiments, sentiment_counts, color=['green', 'gray', 'red'])
+    ax.set_ylabel('Quantidade')
+    ax.set_title('Quantidade de Comentários por Sentimento')
+    st.pyplot(fig)
+    
+    # Gráfico de linha mostrando a evolução dos sentimentos ao longo do tempo
+    st.subheader("Evolução dos Sentimentos ao Longo do Tempo")
+    df['data_comentario'] = pd.to_datetime(df['data_comentario'])
+    sentiment_over_time = df.groupby([df['data_comentario'].dt.date, 'sentimento']).size().unstack().fillna(0)
+    
+    fig, ax = plt.subplots()
+    sentiment_over_time.plot(ax=ax, color=['green', 'gray', 'red'])
+    ax.set_ylabel('Quantidade')
+    ax.set_title('Evolução dos Sentimentos ao Longo do Tempo')
+    st.pyplot(fig)
+
+    
     # Por exemplo: Gráfico de barras para visualizar a quantidade de sentimentos ao longo do tempo
     # (Isso é apenas um esboço. Você precisa implementar o código real para criar o gráfico.)
     st.subheader("Distribuição de sentimentos ao longo do tempo")
@@ -59,44 +82,7 @@ def show(df):
     st.header("Insights")
     st.info("Os comentários positivos têm uma média de curtidas mais alta em comparação com outros sentimentos.")
     
-
-    
-    # Gráficos
-    st.header("Detalhes dos Sentimentos")
-    
-    # Gráfico de Barras
-    st.subheader("Quantidade de Sentimentos")
-    sentiments = ['positivo', 'neutro', 'negativo']
-    sentiment_counts = [len(df[df['sentimento'] == sentiment]) for sentiment in sentiments]
-    
-    fig, ax = plt.subplots()
-    ax.bar(sentiments, sentiment_counts, color=['green', 'gray', 'red'])
-    ax.set_ylabel('Quantidade')
-    ax.set_title('Quantidade de Comentários por Sentimento')
-    
-    st.pyplot(fig)
-    
-    # Gráfico de Linha (Necessário ter dados organizados por data para ser eficaz)
-    st.subheader("Evolução dos Sentimentos ao Longo do Tempo")
-    df['data_comentario'] = pd.to_datetime(df['data_comentario'])
-    sentiment_over_time = df.groupby([df['data_comentario'].dt.date, 'sentimento']).size().unstack().fillna(0)
-    
-    fig, ax = plt.subplots()
-    sentiment_over_time.plot(ax=ax, color=['green', 'gray', 'red'])
-    ax.set_ylabel('Quantidade')
-    ax.set_title('Evolução dos Sentimentos ao Longo do Tempo')
-    
-    st.pyplot(fig)
-    
-    # Gráfico de Torta
-    st.subheader("Distribuição Percentual dos Sentimentos")
-    
-    fig, ax = plt.subplots()
-    ax.pie(sentiment_counts, labels=sentiments, colors=['green', 'gray', 'red'], autopct='%1.1f%%', startangle=90)
-    ax.axis('equal')
-    ax.set_title('Distribuição Percentual dos Sentimentos')
-    
-    st.pyplot(fig)
+    # Outras análises ou gráficos podem ser adicionados conforme necessário
 
     # Final do dashboard com um call-to-action ou uma nota.
     st.write("""
