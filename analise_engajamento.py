0044c335b3ef0dad45bd5dd18f5d9cb3ecf3ebe4
Leandro Gonçalves <leandroaugusto@ioasys.com.br>
2023-08-28 18:24:05 -0300

    1

--- analise_engajamento.py
+++ analise_engajamento.py
@@ -3,10 +3,16 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+sns.set_style("whitegrid")
+
 def show(df):
     st.title("Análise de Engajamento")
     
-    # Visão Geral - Big Numbers
+    st.write("""
+    Explore os indicadores e métricas que refletem o engajamento com as postagens nas redes sociais.
+    """)
+
+    # Visão Geral
     st.header("Visão Geral")
     col1, col2, col3, col4 = st.columns(4)
     
@@ -21,7 +27,7 @@ def show(df):
     with col3:
         st.markdown("**Compartilhamentos**")
         st.markdown(f"<h1 style='text-align: center;'>{df['compartilhamentos'].sum()}</h1>", unsafe_allow_html=True)
-
+    
     with col4:
         st.markdown("**Visualizações**")
         st.markdown(f"<h1 style='text-align: center;'>{df['visualizacoes'].sum()}</h1>", unsafe_allow_html=True)
@@ -29,8 +35,10 @@ def show(df):
     # Engajamento por tipo de postagem
     st.subheader("Engajamento por Tipo de Postagem")
     engagement_by_type = df.groupby('tipo_postagem').sum()[['curtidas', 'reacoes', 'compartilhamentos', 'visualizacoes']]
-    engagement_by_type.plot(kind='bar', figsize=(10, 6), stacked=True)
+    plt.figure(figsize=(10, 6))
+    engagement_by_type.plot(kind='bar', stacked=True)
     plt.title('Engajamento por Tipo de Postagem')
+    plt.ylabel('Total')
     st.pyplot(plt.gcf())
     plt.clf()
 
@@ -38,29 +46,53 @@ def show(df):
     st.subheader("Engajamento ao Longo do Tempo")
     df['data_postagem'] = pd.to_datetime(df['data_postagem'])
     engagement_over_time = df.groupby(df['data_postagem'].dt.date).sum()[['curtidas', 'reacoes', 'compartilhamentos']]
-    engagement_over_time.plot(figsize=(10, 6))
+    plt.figure(figsize=(10, 6))
+    engagement_over_time.plot()
     plt.title('Engajamento ao Longo do Tempo')
+    plt.ylabel('Total')
     st.pyplot(plt.gcf())
     plt.clf()
 
-    # Postagens mais engajadas
-    st.subheader("Top 5 postagens por engajamento total (curtidas + reações + compartilhamentos)")
+    # Top 5 postagens mais engajadas
+    st.subheader("Top 5 Postagens por Engajamento Total (Curtidas + Reações + Compartilhamentos)")
     df['engajamento_total'] = df['curtidas'] + df['reacoes'] + df['compartilhamentos']
     top_engaged_posts = df.sort_values(by='engajamento_total', ascending=False).head(5)
-    for _, row in top_engaged_posts.iterrows():
-        col1, col2, col3 = st.columns(3)
-        with col1:
-            st.markdown(f"**Postagem**: {row['postagem']}")
-        with col2:
-            st.markdown(f"**Engajamento**: {row['engajamento_total']}")
-        with col3:
-            st.image(row['url_imagem'] if row['url_imagem'] else row['url_video'], use_column_width=True)  # Displaying image or video thumbnail
 
-        st.write("---")  # Separator
+    col_style = """
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        padding: 10px;
+    """
+
+    for _, row in top_engaged_posts.iterrows():
+        
+        st.markdown(f"### {row['postagem']}")
+        
+        col1, col2, col3 = st.columns(3)
+        
+        with col1:
+            st.markdown(f"<div style='{col_style}'>", unsafe_allow_html=True)
+            st.markdown("👍", unsafe_allow_html=True)
+            st.markdown(f"**Curtidas**<br>{row['curtidas']}", unsafe_allow_html=True)
+            st.markdown("</div>", unsafe_allow_html=True)
+            
+        with col2:
+            st.markdown(f"<div style='{col_style}'>", unsafe_allow_html=True)
+            st.markdown("🔄", unsafe_allow_html=True)
+            st.markdown(f"**Compartilhamentos**<br>{row['compartilhamentos']}", unsafe_allow_html=True)
+            st.markdown("</div>", unsafe_allow_html=True)
+        
+        with col3:
+            st.markdown(f"<div style='{col_style}'>", unsafe_allow_html=True)
+            st.markdown("👁️", unsafe_allow_html=True)
+            st.markdown(f"**Visualizações**<br>{row['visualizacoes']}", unsafe_allow_html=True)
+            st.markdown("</div>", unsafe_allow_html=True)
+        
+        st.write("---")
 
     st.write("""
     ### Obrigado por explorar a análise de engajamento!
     Use estes insights para otimizar suas estratégias de postagem e melhorar o engajamento.
     """)
-
-# Para usar este módulo, importe-o em seu arquivo principal (app.py) e adicione uma nova condição para a guia de análise de engajamento.
