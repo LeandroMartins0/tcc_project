752d572d078838b4ae2061a28f17c8d682bf3db8
Leandro Gonçalves <leandroaugusto@ioasys.com.br>
2023-09-16 22:42:43 -0300

    ambiente de teste

--- teste/Scripts/activate.bat
+++ teste/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\LeandroAugusto\Desktop\TCC\Project\tcc_project\teste
+set VIRTUAL_ENV=C:\Projects 2\tcc_project\teste
 
 if not defined PROMPT set PROMPT=$P$G
 
